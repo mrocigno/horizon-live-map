769bf24eda952619ddc84e4e21f908065211bd5f
Matheus Rocigno <matheus.rocigno@moving.com.br>
2021-03-07 00:13:40 -0300

    splash animated

--- app/src/main/java/br/com/mrocigno/horizonlivemap/ui/SplashActivity.kt
+++ app/src/main/java/br/com/mrocigno/horizonlivemap/ui/SplashActivity.kt
@@ -3,14 +3,33 @@ package br.com.mrocigno.horizonlivemap.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import br.com.arch.toolkit.delegate.viewProvider
 import br.com.mrocigno.horizonlivemap.R
 import br.com.mrocigno.horizonlivemap.map.ui.map.MapActivity
 
 class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
 
+    private val motionLayout: MotionLayout by viewProvider(R.id.motion_layout)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MapActivity::class.java))
-        finish()
+        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) = Unit
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) = Unit
+
+            override fun onTransitionCompleted(p0: MotionLayout?, state: Int) {
+                if (state == R.id.end) {
+                    startActivity(Intent(this@SplashActivity, MapActivity::class.java))
+                    finish()
+                }
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) = Unit
+        })
+        motionLayout.postDelayed({
+            motionLayout.transitionToState(R.id.end)
+        }, 1000)
     }
 }
