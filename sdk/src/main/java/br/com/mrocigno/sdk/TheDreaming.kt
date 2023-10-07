3c23ca0d0565bf0f42ab4e573bbc7bfbe468401e
mrocigno <rocignom@gmail.com>
2023-10-07 18:29:36 -0300

    bigbrother update

--- sdk/src/main/java/br/com/mrocigno/sdk/TheDreaming.kt
+++ sdk/src/main/java/br/com/mrocigno/sdk/TheDreaming.kt
@@ -1,6 +1,7 @@
 package br.com.mrocigno.sdk
 
 import br.com.mrocigno.bigbrother.core.BigBrotherProvider
+import br.com.mrocigno.bigbrother.database.addDatabasePage
 import br.com.mrocigno.bigbrother.log.addLogPage
 import br.com.mrocigno.bigbrother.network.addNetworkPage
 
@@ -9,6 +10,7 @@ class TheDreaming : BigBrotherProvider() {
     override val isEnabled: Boolean = true
 
     override fun setupPages() {
+        addDatabasePage()
         addNetworkPage()
         addLogPage()
     }
